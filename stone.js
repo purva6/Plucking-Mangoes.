d18808782b744612073f23e543998e4aebf7a826
purva6 <81624972+purva6@users.noreply.github.com>
2021-04-01 21:16:17 +0530

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,21 @@
+class Stone{
+    constructor(x, y,diametre) {
+        var options = {
+           isStatic:false,
+           restitution:0,
+           friction:1,
+           density:1.2
+        }
+        this.body = Bodies.circle(x, y, diametre, options);
+        this.diametre=diametre;
+        this.image = loadImage("stone.png");
+        World.add(world, this.body);
+      }
+      display(){
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.diametre);
+        pop();
+      }
+}
